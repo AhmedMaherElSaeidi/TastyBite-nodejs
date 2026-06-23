1e0387e841aff4bb0c8cd54b4a517cf220775682
Ahmed Maher <ahmedelseaidy22@gmail.com>
2026-06-23 14:08:29 +0300

    Adding jwt token expiration time as .env variable

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-  return jwt.sign({ id: user._id, role: user.role.en }, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign({ id: user._id, role: user.role.en }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXP });
 };
